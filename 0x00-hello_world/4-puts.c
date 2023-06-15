a0fd95113543e4a1f4b4655ab1b90efa612bbba7
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-15 13:34:41 -0700

    Task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- main- Entry point 
-
- Return: Always 0(Success)
- */
+* main- Entry point 
+*
+*Return: Always 0(Success)
+*/
  int main() {
 	 puts("Programming is like building a multilingual puzzle\n");
-	 return 0;
+	 return (0);
  }
