31ca07a94f3ab66b425c3b3cbbd852e37f761e94
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-16 03:29:51 -0700

    Task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-  *main- prints "with proper grammar, but the outcome is a piece of art," newline follows
-  *
+  *mainu- prints "with proper grammar, but the outcome is a piece of art,"
+  *newline follows
   *Return: Always 0(Success)
   */
 int main(void)
