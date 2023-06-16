121cdc685a6c5ead5dc10553b5c915277410fee7
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-16 05:03:49 -0700

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,15 @@
   */
 int main(void)
 {
-	printf("Size od char: %zu bytes\n", sizeof(char));
-	printf("Size of int: %zu bytes\n", sizeof(int));
-	printf("Size of float: %zu bytes\n", sizeof(float));
-	printf("Size of double: %zu bytes\n", sizeof(double));
+	char a = 1 bytes
+	int b = 4 bytes
+	float c = 4 bytes
+	double d = 8 bytes
+
+	printf("Size of char: %zu bytes\n", sizeof(a));
+	printf("Size of int: %zu bytes\n", sizeof(b));
+	printf("Size of float: %zu bytes\n", sizeof(c));
+	printf("Size of double: %zu bytes\n", sizeof(d));
 	printf("Size of short: %zu bytes\n", sizeof(short));
 	printf("Size of long: %zu bytes\n", sizeof(long));
 	printf("Size of long long: %zu bytes\n", sizeof(long long));
