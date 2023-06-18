12767d800c416547d6b009e4d3b115a99407dad5
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-18 09:01:03 -0700

    Task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,13 +10,13 @@ int main(void)
 
 	if (n > 0)
 	{
-		printf("\"n is positive);
-	}else if (n = o)
+		printf("\"n is positive");
+	}else if (n = 0)
 	{
-		printf("\"n is zero);
+		printf("\"n is zero");
 	}else
 	{
-		printf("\"n is negative);
+		printf("\"n is negative");
 	}
 
 	return (0);
