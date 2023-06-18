389d6b56ac6efe514379a403f53841ed3b2d5b3d
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-18 08:50:30 -0700

    Task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+  *main-Entry point
+  *Return: Always 0(Success)
+  */
+int main(void)
+{
+	int = 6;
+
+	if (n > 0)
+	{
+		printf("\"n is positive);
+	}else if (n = o)
+	{
+		printf("\"n is zero);
+	}else
+	{
+		printf("\"n is negative);
+	}
+
+	return (0);
+}
