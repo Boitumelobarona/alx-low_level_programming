b7612c2eacfaee46d5c616c79314263dbb8c0745
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-19 05:16:34 -0700

    Task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
   *main-Entry point
-  *Return: 1 (Sucess)
+  *Return: 1 (Success)
   */
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015 - 10 - 19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 		write(1, quo, 59);
 		return (1);
 }
