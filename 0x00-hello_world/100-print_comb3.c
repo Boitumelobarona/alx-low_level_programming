b954d4e66215f4cce1375094cdc6e19dfb3333b5
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-19 05:53:21 -0700

    Task8

---
+++ 0x00-hello_world/100-print_comb3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+  *main-Entry point
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (n = 1; n < 10; n++)
+		{
+			putchar("%d%d\n", i, j);
+		}
+	}
+	return (0);
+}
