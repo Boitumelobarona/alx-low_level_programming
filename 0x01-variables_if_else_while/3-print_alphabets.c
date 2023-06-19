78e2be6f1b308bd160c8e139811836640771321c
Boitumlobarona <Boitumelobaronamohlala@gmail.com>
2023-06-18 19:20:52 -0700

    Task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,15 @@
 int main(void)
 {
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alp1[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alp[i]);
+	} for (i = 0; i < 26; i++)
+	{
+		putchar(alp1[i]);
 	}
 	putchar('\n');
 	return (0);
